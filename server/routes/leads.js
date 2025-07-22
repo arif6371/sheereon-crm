c6f819db91c3f831142394a17165619df1c42232
Arif Khan <143376581+arif6371@users.noreply.github.com>
2025-07-22 15:46:20 +0530

    Updated leads.js

--- server/routes/leads.js
+++ server/routes/leads.js
@@ -222,7 +222,7 @@ router.put('/:id/status', authenticateToken, async (req, res) => {
     }
 
     // Check permissions
-    if (req.user.role === 'BDE' && lead.assignedTo?.toString() !== req.user._id.toString()) {
+    if (req.user.role === 'SALES' && lead.assignedTo?.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: 'You can only update leads assigned to you' });
     }
 
@@ -318,7 +318,7 @@ router.put('/:id', authenticateToken, async (req, res) => {
     }
 
     // Check permissions
-    if (req.user.role === 'BDE' && lead.assignedTo?.toString() !== req.user._id.toString()) {
+    if (req.user.role === 'SALES' && lead.assignedTo?.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: 'You can only update leads assigned to you' });
     }
 
@@ -367,7 +367,7 @@ router.post('/:id/notes', authenticateToken, async (req, res) => {
     }
 
     // Check permissions
-    if (req.user.role === 'BDE' && lead.assignedTo?.toString() !== req.user._id.toString()) {
+    if (req.user.role === 'SALES' && lead.assignedTo?.toString() !== req.user._id.toString()) {
       return res.status(403).json({ message: 'You can only add notes to leads assigned to you' });
     }
 
